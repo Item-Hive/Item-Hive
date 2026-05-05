c1b4e69e739b58de3b9127cc41dca20c7d0e4202
Athini Ngquke <222352302@mycput.ac.za>
2026-05-05 21:03:47 +0200

    new iservice classes

---
+++ Back-end work/service/iservice/IInvoiceService.java
@@ -0,0 +1,14 @@
+package za.ac.cput.branded.service;
+
+import za.ac.cput.branded.domain.Invoice;
+
+import java.util.List;
+
+public interface IInvoiceService {
+    Invoice create(Invoice invoices);
+    Invoice read(String id);
+    Invoice update(Invoice invoices);
+    List<Invoice> getAll();
+    void delete(String id);
+
+}

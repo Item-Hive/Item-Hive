366ba848417fcc5b7a5e658c58ebdba15ed12cb9
SIPHOKUHLE NYANA <218123191@mycput.ac.za>
2026-09-16 21:07:37 +0200

    Add new allowed origin for CORS configuration

--- backend/src/main/java/za/ac/cput/branded/config/CorsConfig.java
+++ backend/src/main/java/za/ac/cput/branded/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/api/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://your-frontend.onrender.com"
+                                "https://item-hive.vercel.app/"
                         )
                         .allowedMethods(
                                 "GET",

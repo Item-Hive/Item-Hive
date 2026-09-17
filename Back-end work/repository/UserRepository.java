3afb6a00294d02a729358bb2abec2907eb366082
Athini Ngquke <222352302@mycput.ac.za>
2026-09-17 20:24:37 +0200

    Delete Back-end work/repository/UserRepository.java

--- Back-end work/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package za.ac.cput.branded.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import za.ac.cput.branded.domain.User;
-
-public interface UserRepository extends JpaRepository<User,String> {
-}

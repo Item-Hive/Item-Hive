ac3d18a62dad7b3bd8e869442f9b607ed197eafa
Athini Ngquke <222352302@mycput.ac.za>
2026-09-26 04:36:49 +0200

    Add files via upload

---
+++ backend/src/main/java/za/ac/cput/branded/repository/ReportRefundRepository.java
@@ -0,0 +1,7 @@
+package za.ac.cput.branded.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import za.ac.cput.branded.domain.ReportRefund;
+
+public interface ReportRefundRepository extends JpaRepository<ReportRefund, String> {
+}

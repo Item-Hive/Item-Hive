433a584b24ea4a3188043d59f6f0267c908d4c2d
Athini Ngquke <222352302@mycput.ac.za>
2026-09-26 06:00:53 +0200

    Update API endpoint from '/api/reportrefund' to '/api/report-refund'

--- backend/src/main/java/za/ac/cput/branded/controller/ReportRefundController.java
+++ backend/src/main/java/za/ac/cput/branded/controller/ReportRefundController.java
@@ -7,7 +7,7 @@ import za.ac.cput.branded.service.ReportRefundService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/reportrefund")
+@RequestMapping("/api/report-refund")
 public class ReportRefundController {
     private ReportRefundService service;
 

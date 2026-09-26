c172595d9d0df609103a0c26fb54d16ca8be5162
OlwethuMtwazi <230036937@mycput.ac.za>
2026-09-26 13:35:54 +0200

    fix: approutes directory for report refund

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -8,7 +8,7 @@ import Login from "../pages/Login";
 import Profile from "../pages/Profile";
 import Checkout from "../pages/Checkout";
 import BrandGuidelines from "../pages/BrandGuidelines";
-import ReportRefund from "..ReportRefund/pages/ReportRefund";
+import ReportRefund from "../pages/ReportRefund";
 
 
 function AppRoutes() {
